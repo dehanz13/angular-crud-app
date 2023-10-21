5696ed8e19f74de5de9c8998acd392e8cf5ab5be
Danniel Hansel <dehanz13@users.noreply.github.com>
2023-10-21 16:22:47 -0500

    add checkout form model

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from 'src/app/services/cart.service';
 
@@ -10,5 +11,21 @@ import { CartService } from 'src/app/services/cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  })
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  // This method allows users to submit their name and address. Then clears the cart.
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn("Your order has been submitted!", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
